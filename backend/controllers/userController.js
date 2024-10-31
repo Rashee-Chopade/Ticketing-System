716eedfd127550fe87fc9e3ee6adb6c8cb6d4a21
Rashee-Chopade <rashchop2001@gmail.com>
2024-10-31 12:22:43 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,15 +4,6 @@ const bcrypt = require('bcryptjs') // A library to help you hash passwords.
 
 const User = require('../models/userModel')
 
-// @desc    Register a new user
-// @route   /api/users
-// @access  Public
-
-/**
- * 'asyncHandler' is a simple middleware for handling exceptions
- * inside of async express routes and passing them to your express
- * error handlers.
- */
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body // destructure the request body params
 
@@ -22,7 +13,6 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('Please provide all required fields')
   }
 
-  // Check for existing user
   const userExists = await User.findOne({ email })
 
   if (userExists) {
@@ -30,18 +20,15 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('User already exists')
   }
 
-  // Hash password
   const salt = await bcrypt.genSalt(10) // 10 is the number of rounds
   const hashedPassword = await bcrypt.hash(password, salt) // hash the password
 
-  // Create user
   const user = await User.create({
     name,
     email,
     password: hashedPassword
   })
 
-  // User is created
   if (user) {
     res.status(201).json({
       _id: user._id,
@@ -55,15 +42,11 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc    Login a user
-// @route   /api/users/login
-// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body // destructuring
 
   const user = await User.findOne({ email })
 
-  // Check User and Password match
   if (user && (await bcrypt.compare(password, user.password))) {
     res.status(200).json({
       _id: user._id,
@@ -72,14 +55,11 @@ const loginUser = asyncHandler(async (req, res) => {
       token: generateToken(user._id)
     })
   } else {
-    res.status(401) // Unauthorized
+    res.status(401) 
     throw new Error('Invalid credentials')
   }
 })
 
-// @desc    Get current user
-// @route   /api/users/me
-// @access  Private
 const getMe = asyncHandler(async (req, res) => {
   const user = {
     id: req.user._id,
@@ -89,7 +69,6 @@ const getMe = asyncHandler(async (req, res) => {
   res.status(200).json(user)
 })
 
-// Generate token
 generateToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '30d'
